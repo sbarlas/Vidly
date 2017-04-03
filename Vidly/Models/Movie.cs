31d7c7d1274ae2cec0c47bf825b4327b420bc9ec
sbarlas <sbarlas@SBDEVWIN10>
2017-04-03 09:24:46 +0100

    Update the model for Movie

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,6 +9,8 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
+
+        public DateTime ReleaseDate { get; set; }
+
     }
 }
