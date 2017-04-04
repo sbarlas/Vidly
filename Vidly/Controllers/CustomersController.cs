0c40af7af7f31741addd44576e0f9465c9b0143d
sbarlas <sbarlas@SBDEVWIN10>
2017-04-04 17:28:54 +0100

    Added in edit customer and new customer logic

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -50,12 +50,59 @@ namespace Vidly.Controllers
         {
             var membershipType = _context.MembershipTypes.ToList();
 
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 MembershipTypes = membershipType
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+               // TryUpdateModel(customerInDb);
+                
+                customerInDb.Name = customer.Name;
+                customerInDb.DateOfBirth = customer.DateOfBirth;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+
+            var customerDetails = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if(customerDetails == null)
+            {
+                return HttpNotFound("Can not find customer");
+            }
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customerDetails,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            
+            return View("CustomerForm", viewModel);
         }
 
         //private List<Customer> GetStaticCustomerList()
