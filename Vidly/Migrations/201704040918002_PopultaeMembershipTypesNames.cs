8d6aa8edfc6e623b2f6f36d457f4bdb17940dbfd
sbarlas <sbarlas@SBDEVWIN10>
2017-04-04 10:33:23 +0100

    Testing commit

--- Vidly/Migrations/201704040918002_PopultaeMembershipTypesNames.cs
+++ Vidly/Migrations/201704040918002_PopultaeMembershipTypesNames.cs
@@ -10,7 +10,7 @@ namespace Vidly.Migrations
             Sql("UPDATE MembershipTypes SET MembershipName = 'Pay As You Go' WHERE Id = 1");
             Sql("UPDATE MembershipTypes SET MembershipName = 'Monthly' WHERE Id = 2");
             Sql("UPDATE MembershipTypes SET MembershipName = 'Quarterly' WHERE Id = 3");
-            Sql("UPDATE MembershipTypes SET MembershipName = 'Annually' WHERE Id = 4");
+            Sql("UPDATE MembershipTypes SET MembershipName = 'Annually' WHERE Id = 4");  
         }
         
         public override void Down()
