49a92d46d5f30ad759f4a82fd481f9eb0976b28f
sbarlas <sbarlas@SBDEVWIN10>
2017-04-04 10:21:13 +0100

    Added in data and membership types

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public int Age { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
         public bool IsActive { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeId { get; set; }
     }
 
 }
